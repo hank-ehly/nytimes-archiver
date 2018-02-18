dfb8be902ff5c7cb0263606adf557de8131c5ae1
Hank Ehly <henry.ehly@gmail.com>
2018-02-18 17:55:47 +0900

    Avoid sqlalchemy.exc.ProgrammingError

--- archive.py
+++ archive.py
@@ -4,8 +4,9 @@ import pandas.io.json.normalize
 import json
 from sqlalchemy import create_engine
 from settings import NYTIMES_ARCHIVE_API_KEY, DB_NAME, DB_TABLE, DB_PORT, DB_USER, DB_PASS, DB_HOST
+import os
 
-OUT_PATH = '2018_1.csv'
+TEMP_CSV_PATH = 'temp.csv'
 
 
 def fetch_archive_data():
@@ -17,12 +18,16 @@ def fetch_archive_data():
     doc_list = dict_response['response']['docs']
     keep_columns = ['_id', 'byline.person', 'document_type', 'headline.main', 'pub_date', 'web_url', 'word_count']
 
-    return pd.io.json.json_normalize(doc_list)[keep_columns]
+    # to avoid sqlalchemy.exc.ProgrammingError
+    pd.io.json.json_normalize(doc_list)[keep_columns].to_csv(TEMP_CSV_PATH)
+
+    return pd.read_csv(TEMP_CSV_PATH)
 
 
 if __name__ == '__main__':
     connection = create_engine('mysql+mysqldb://%s:%s@%s:%i/%s' % (DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME))
 
     df = fetch_archive_data()
-    df.to_sql(name=DB_TABLE, con=connection, if_exists='append', index=False)
+    df.to_sql(name=DB_TABLE, con=connection, if_exists='append')
 
+    os.unlink(TEMP_CSV_PATH)
